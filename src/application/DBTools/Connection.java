84ffa27e58b7d98491e13ca10b73faa6830cc47d
Haxxflaxx <daniel.arnqvist@gmail.com>
2015-11-02 21:50:26 +0100

    Name change dbTools
    Changed the name of dbTools lib to conform to conventions

--- src/application/DBTools/Connection.java
+++ src/application/DBTools/Connection.java
@@ -1,4 +1,4 @@
-package application.DBTools;
+package application.dbTools;
 
 
 
--- src/application/DBTools/Query.java
+++ src/application/DBTools/Query.java
@@ -1,4 +1,4 @@
-package application.DBTools;
+package application.dbTools;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
