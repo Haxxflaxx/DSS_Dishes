ffee97b8a03dca81b6958b11af724eacb48d6bbf
haxxflaxx <daniel.arnqvist@gmail.com>
2015-11-03 15:18:50 +0100

    GUI functionality update
    Added on startup update of recipe list in main view.
    Added .db to database file

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -1,13 +1,29 @@
 package application.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
+import javafx.scene.control.ListView;
+
+import application.dbTools.*;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 /**
  * Main controller class for the layout.
  */
-public class MainController {
+public class MainController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        updateRecipList();
+    }
 
     /** Holder of a switchable vista. */
     @FXML
@@ -21,4 +37,27 @@ public class MainController {
     public void setVista(Node node) {
         vistaHolder.getChildren().setAll(node);
     }
+
+    /** List holding the recipe names from the DB. */
+    @FXML
+    private ListView recipList;
+
+    public void updateRecipList(){
+        ArrayList<ArrayList<String>> dataSet;
+        ObservableList<String> itemList = FXCollections.observableArrayList();
+
+        try {
+            dataSet = Query.fetchData("recipes", "Name");
+
+            for (ArrayList<String> element : dataSet){
+                itemList.add(element.get(0));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        recipList.setItems(itemList);
+    }
+
+
 }
+
