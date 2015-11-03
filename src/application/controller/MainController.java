bad6c8cc7381e837882b535f0ba50e0d00c0fdc8
Haxxflaxx <daniel.arnqvist@gmail.com>
2015-11-03 23:42:05 +0100

    Main view search
    Implemented simple search function for the recipe list.
    * Search takes a string and adds wildcard to the end.
    * Empty search field returns full list.
    
    Should there be a wildcard before the search string to?

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -5,6 +5,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.control.ListView;
 
@@ -33,6 +34,12 @@ public class MainController implements Initializable {
     /** Holder of a switchable vista. */
     @FXML private StackPane vistaHolder;
 
+    /** List holding the recipe names from the DB. */
+    @FXML protected ListView recipList;
+
+    /** Field for entering search criteria. */
+    @FXML private TextField recipSearch;
+
     /**
      * Replaces the vista displayed in the vista holder with a new vista.
      *
@@ -42,9 +49,10 @@ public class MainController implements Initializable {
         vistaHolder.getChildren().setAll(node);
     }
 
-    /** List holding the recipe names from the DB. */
-    @FXML protected ListView recipList;
 
+    /**
+     * Loads the name of all recipes and puts them into recipList.
+     */
     public void updateRecipList(){
         ArrayList<ArrayList<String>> dataSet;
         ObservableList<String> itemList = FXCollections.observableArrayList();
@@ -61,10 +69,35 @@ public class MainController implements Initializable {
         recipList.setItems(itemList);
     }
 
-    public void openWelcome(){
-        VistaNavigator.loadVista(
-                VistaNavigator.WELCOME
-        );
+    /**
+     * Loads the name of all recipes and puts them into recipList.
+     *
+     * @param search input for selection based on name with wildcard at the end.
+     */
+    public void updateRecipList(String search){
+        ArrayList<ArrayList<String>> dataSet;
+        ObservableList<String> itemList = FXCollections.observableArrayList();
+        String condition = "Name LIKE '" + search + "%'";
+        System.out.println(condition);
+
+        try {
+            dataSet = Query.fetchData("recipes", "Name", condition);
+
+            for (ArrayList<String> element : dataSet){
+                itemList.add(element.get(0));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        recipList.setItems(itemList);
+    }
+
+    /**
+     * Run search recipe based on name.
+     */
+    public void search(){
+        System.out.println(recipSearch.getText());
+        updateRecipList(recipSearch.getText());
     }
 
 
