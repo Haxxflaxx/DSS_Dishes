589c6cd9adf03286f8b9ca4bc987a8347e9474f6
Pierre Leveau <pierre.leveau87@gmail.com>
2015-11-09 11:24:52 +0100

    no message

--- src/application/controller/LoginController.java
+++ src/application/controller/LoginController.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.MenuItem;
 
 import java.awt.*;
 import java.net.URL;
