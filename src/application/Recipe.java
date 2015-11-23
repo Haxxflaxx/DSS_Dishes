13ef4fe3ef5cc82c9acdf9045dbf8dde7501ad11
haxxflaxx <daniel.arnqvist@gmail.com>
2015-11-23 10:57:09 +0100

    Updated recipe class wit update method.
    Updates based on the input ID.

--- src/application/Recipe.java
+++ src/application/Recipe.java
@@ -1,5 +1,10 @@
 package application;
 
+import application.dbTools.Query;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  * Created by haxxflaxx on 2015-11-09.
  *
@@ -38,6 +43,32 @@ public class Recipe {
 
     public static void setSelected(Recipe selected) {
         Recipe.selected = selected;
+    }
+
+    /**
+     * Updates the info in selected with a recipe based on ID.
+     * @param ID The ID of the chosen recipe.
+     */
+    public static void setSelectedById(String ID) {
+        try {
+            String condition = "ID='" + ID + "'";
+            ArrayList<ArrayList<String>> dataSet = Query.fetchData("recipes", "*", condition);
+
+            selected = new Recipe(
+                    dataSet.get(0).get(0),
+                    dataSet.get(0).get(1),
+                    dataSet.get(0).get(2),
+                    dataSet.get(0).get(3),
+                    dataSet.get(0).get(4),
+                    dataSet.get(0).get(5),
+                    dataSet.get(0).get(6),
+                    dataSet.get(0).get(7),
+                    dataSet.get(0).get(8),
+                    dataSet.get(0).get(9)
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getId() {
